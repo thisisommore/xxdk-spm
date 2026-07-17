959918312e6a1d6ecfc43497ab9b42927489475d
Om More <51229945+thisisommore@users.noreply.github.com>
2026-07-17 12:56:06 +0530

    Update Bindings binary target URL to version 0.0.3

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Bindings",
-            url: "https://github.com/thisisommore/xxdk-spm/releases/download/0.0.2/Bindings.xcframework.zip",
+            url: "https://github.com/thisisommore/xxdk-spm/releases/download/0.0.3/Bindings.xcframework.zip",
             checksum: "7248ef90a4b807359b291b6c7a2f7821945e6907a2ceb989aa9013b11b2cd41a"
         ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
