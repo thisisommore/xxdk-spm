d35f3a00348ba2009080425b67c66355cc27a002
Om More <51229945+thisisommore@users.noreply.github.com>
2026-07-17 13:21:15 +0530

    Update macOS platform version to v26

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XXDK",
-    platforms: [.iOS(.v17),.macOS(.v14)],
+    platforms: [.iOS(.v17),.macOS(.v26)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
